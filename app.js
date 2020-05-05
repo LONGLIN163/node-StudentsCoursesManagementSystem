b9e368aa04411363107077b6b65988397758cd2e
linlong <jianjiandandan.lin@163.com>
2020-05-05 20:12:15 +0200

    test2

--- app.js
+++ app.js
@@ -1,2 +1,29 @@
 var express=require("express");
+var mongoose=require("mongoose");
+var session=require("express-session");
+
+
+//Create express app obj
 var app = express();
+//Connect to database
+mongoose.connect('mongodb://localhost/ElectiveCoursesSystemDB', {useNewUrlParser: true});
+
+//Set model engine
+app.set("view engine","ejs");
+
+
+//Create middlewares
+// app.get('/', function(req, res){
+//     res.send('Hello World');
+// });
+
+//Set static file
+app.use(express.static("public"));
+//Set 404 info page
+app.use(function(req,res){
+    res.send("404! The page doesn't exist!!!");
+})
+
+
+app.listen(3000);
+console.log("The app is running on 3000!")
