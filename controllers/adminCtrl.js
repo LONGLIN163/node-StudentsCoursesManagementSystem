a3d24640b722c7f105b7cf0e4f486839117dc7e7
linlong <jianjiandandan.lin@163.com>
2020-05-12 12:38:13 +0200

    upload excel file complete

--- controllers/adminCtrl.js
+++ controllers/adminCtrl.js
@@ -1,5 +1,7 @@
 var formidable = require('formidable');
-
+var path = require("path");
+var fs = require("fs");
+var xlsx =require('node-xlsx');
 
 exports.showAdminDashboard=function(req,res){
     res.render("admin/index.ejs",{
@@ -30,12 +32,44 @@ exports.doAdminStudentImport=function(req,res){
     form.uploadDir = "./uploads";
     form.keepExtensions = true;
     form.parse(req, function(err, fields, files) {
-    //   res.writeHead(200, {'content-type': 'text/plain'});
-    //   res.write('received upload:\n\n');
-    //   res.end(util.inspect({fields: fields, files: files}));
+        //console.log("------",files.studentExcel)
+        if(!files.studentExcel.name){
+            res.send("Please choose a file to upload!");
+            return;
+        }else if(path.extname(files.studentExcel.name)!=".xlsx"){
+            //delete the illegal file
+            fs.unlink("./"+files.studentExcel.path,function(err){
+                if(err){
+                    console.log("Fail to delete file!");
+                    return;
+                }
+                res.send("Uploading illegal file, the file has been deleted from server!");
+            });
+            return;
+        }
+        //read this .xlsx file,this ia a sync statement
+        var workSheetsFromFile = xlsx.parse("./"+files.studentExcel.path);
+        //Check if the array match 
+        //console.log(workSheetsFromFile.length)
+        if(workSheetsFromFile.length!=6){
+            res.send("U r missing sub tables!!!");
+            return;
+        }
+        for(var i=0;i<6;i++){
+            if(
+                workSheetsFromFile[i].data[0][0]!="sid"||
+                workSheetsFromFile[i].data[0][1]!="sname"
+                ){
+                res.send("No."+i +" table's th is incorrect!");
+                return;
+            }
+        }
+
+
+        res.send("Upload success!!!")
+
     });
 
-    console.log("ok");
 }
 //-------------------------------- 
 exports.showAdminCourse=function(req,res){
