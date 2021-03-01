b55b106033655a9f5c90d1831601c06c1d75114b
developerlin123 <developerlin123@gmail.com>
2021-03-01 17:47:50 +0100

    adjust UI

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
 var express=require("express");
 var mongoose=require("mongoose");
 var session=require("express-session");
-// 
+
 var adminCtrl=require("./controllers/adminCtrl");
-
-
 //Create express app obj
 var app = express();
 //Connect to database
-mongoose.connect('mongodb://localhost/ElectiveCoursesSystemDB', {useNewUrlParser: true});
+mongoose.connect('mongodb://localhost/studnetCourseSystemDB', {useNewUrlParser: true});
 //Use session
 app.use(session({
-    secret: 'ElectiveCoursesSystemDB',
+    secret: 'studnetCourseSystemDB',
     resave: false,
     saveUninitialized: true,
     cookie: { secure: true }
@@ -19,6 +17,7 @@ app.use(session({
 
 //Set model engine
 app.set("view engine","ejs");
+//Middlewares,routers
 app.get('/admin',adminCtrl.showAdminDashboard);
 app.get('/admin/student',adminCtrl.showAdminStudent);
 app.get('/admin/student/import',adminCtrl.showAdminStudentImport);
@@ -31,9 +30,10 @@ app.get('/admin/statement',adminCtrl.showAdminStatement);
 //Set static file
 app.use(express.static("public"));
 //Set 404 info page
+/*
 app.use(function(req,res){
-    res.send("404A! The page doesn't exist!!!");
-})
+    res.send("404! The page doesn't exist!!!");
+})*/
 
 
 app.listen(3000);
