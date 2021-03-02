0bde10b4afa7ca4e839086dbdf100c7095ed35b7
developerlin123 <developerlin123@gmail.com>
2021-03-02 15:16:33 +0100

    modify upload excel logic

--- controllers/adminCtrl.js
+++ controllers/adminCtrl.js
@@ -1,5 +1,5 @@
 var formidable = require('formidable');
-var path = require("path");
+var path = require("path");// use for get extensions
 var fs = require("fs");
 var xlsx =require('node-xlsx');
 var Student = require("../models/Student");
@@ -38,7 +38,7 @@ exports.doAdminStudentImport=function(req,res){
         if(!files.studentExcel.name){
             res.send("Please choose a file to upload!");
             return;
-        }else if(path.extname(files.studentExcel.name)!=".xlsx"){
+        }else if(path.extname(files.studentExcel.name)!=".xlsx"){// file type validation by extension
             //delete the illegal file
             fs.unlink("./"+files.studentExcel.path,function(err){
                 if(err){
@@ -49,18 +49,19 @@ exports.doAdminStudentImport=function(req,res){
             });
             return;
         }
-        //read this .xlsx file,this ia a sync statement
+        //read this .xlsx file use plugin'node-xlsx',this ia a sync statement
         var workSheetsFromFile = xlsx.parse("./"+files.studentExcel.path);
         //Check if the array match 
-        console.log(workSheetsFromFile)
+        //console.log("workSheetsFromFile:",workSheetsFromFile)
+        console.log("workSheetsFromFileLen:",workSheetsFromFile.length)
 
         if(workSheetsFromFile.length!=6){
             res.send("U r missing sub tables!!!");
             return;
         }
         for(var i=0;i<6;i++){
-            console.log("----"+workSheetsFromFile[i].data[0][0]);
-            console.log("----"+workSheetsFromFile[i].data[0][1]);
+            console.log("0---"+workSheetsFromFile[i].data[0][0]);
+            console.log("1---"+workSheetsFromFile[i].data[0][1]);
             if(
                 workSheetsFromFile[i].data[0][0]!="sid"||
                 workSheetsFromFile[i].data[0][1]!="name"
