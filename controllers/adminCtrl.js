622598c3d428f2e9674dfd6e34ad2601b3f68d4d
developerlin123 <developerlin123@gmail.com>
2021-03-08 09:52:10 +0100

    course integration 1

--- controllers/adminCtrl.js
+++ controllers/adminCtrl.js
@@ -6,12 +6,6 @@ exports.showAdminDashboard=function(req,res){
     });
 }
 
-exports.showAdminCourse=function(req,res){
-    res.render("admin/course.ejs",{
-        page:"course"
-    });
-}
-
 exports.showAdminReport=function(req,res){
     res.render("admin/report.ejs",{
         page:"report"
